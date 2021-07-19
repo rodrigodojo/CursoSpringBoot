999ad0e87381b6804f6481d7c911e314fde7d569
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-07-19 17:48:34 -0300

    Criando a classe de ItemPedido e suas implementações

--- src/main/java/com/rodrigodojo/cursomc/domain/Produto.java
+++ src/main/java/com/rodrigodojo/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.rodrigodojo.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -32,6 +34,8 @@ public class Produto implements Serializable{
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 		
 	}
@@ -40,6 +44,14 @@ public class Produto implements Serializable{
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -73,6 +85,14 @@ public class Produto implements Serializable{
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -98,8 +118,5 @@ public class Produto implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
 
 }
